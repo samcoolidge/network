b3f62dfeb826fd113367b21592c1f823bbdeee25
samcoolidge <sam.r.coolidge@gmail.com>
2016-08-09 15:31:36 -0400

    Commit 8/9 2

--- setup.py
+++ setup.py
@@ -16,20 +16,7 @@ Created on Mon Jul 11 14:15:07 2016
 from distutils.core import setup
 from Cython.Distutils import Extension
 from Cython.Distutils import build_ext
-#from Cython.Build import cythonize
-import cython_gsl
 import numpy
-'''
-setup(
-    include_dirs = [cython_gsl.get_include(), numpy.get_include()],
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("Fast_log12",
-                             ["Fast_log12.pyx"],
-                             libraries=cython_gsl.get_libraries(),
-                             library_dirs=[cython_gsl.get_library_dir()],
-                             include_dirs=[cython_gsl.get_cython_include_dir()])]
-    )
-'''
 
 from distutils.core import setup, Extension 
 from Cython.Build import cythonize
